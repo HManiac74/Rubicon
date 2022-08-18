e3040c47d4427829bea4fc147011ec2205a48d65
HManiac74 <hmaniac28@yahoo.de>
2022-08-18 20:55:20 +0200

    Update main.rs
    clear screen

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
-use crossterm::event::{Event, KeyCode, KeyEvent};
-use crossterm::{event, terminal};
+use crossterm::event::*;
+use crossterm::terminal::ClearType;
+use crossterm::{event, execute, terminal};
+use std::io::stdout;
 use std::time::Duration;
 
 struct CleanUp;
@@ -26,11 +28,12 @@ impl Reader {
 
 struct Editor {
     reader: Reader,
+    output: Output,
 }
 
 impl Editor {
     fn new() -> Self {
-        Self {reader: Reader}
+        Self {reader: Reader, output: Output::new()}
     }
 
     fn process_keypress(&self) -> crossterm::Result<bool> {
@@ -45,7 +48,24 @@ impl Editor {
     }
 
     fn run(&self) -> crossterm::Result<bool> {
+        self.output.refresh_screen()?;
         self.process_keypress()
+    }
+}
+
+struct Output;
+
+impl Output {
+    fn new() -> Self {
+        Self
+    }
+
+    fn clear_screen() -> crossterm::Result<()> {
+        execute!(stdout(), terminal::Clear(ClearType::All))
+    }
+
+    fn refresh_screen(&self) -> crossterm::Result<()> {
+        Self::clear_screen()
     }
 }
 
