74e89a150befcec58de102f5fffde185639659a7
HManiac74 <hmaniac28@yahoo.de>
2022-08-20 12:20:38 +0200

    Update main.rs
    PgUp, PgDwn, Home, End

--- src/main.rs
+++ src/main.rs
@@ -1,137 +1,63 @@
 use crossterm::event::*;
 use crossterm::terminal::ClearType;
 use crossterm::{cursor, event, execute, queue, terminal};
-use std::fmt::format;
-use std::io::{self, stdout, Write};
+use std::io;
+use std::io::{stdout, Write};
 use std::time::Duration;
+
+const VERSION: &str = "0.2";
 
 struct CleanUp;
 
 impl Drop for CleanUp {
     fn drop(&mut self) {
-        terminal::disable_raw_mode().expect("Could not disable raw mode");
-        Output::clear_screen().expect("Error");
+        terminal::disable_raw_mode().expect("Unable to disable raw mode");
+        Output::clear_screen().expect("error");
     }
 }
 
-struct Reader;
-
-impl Reader {
-    fn read_key(&self) -> crossterm::Result<KeyEvent> {
-        loop {
-            if event::poll(Duration::from_millis(500))? {
-                if let Event::Key(event) = event::read()? {
-                    return Ok(event);
-                }
-            }
-        }
-    }
+struct CursorController {
+    cursor_x: usize,
+    cursor_y: usize,
+    screen_rows: usize,
+    screen_columns: usize,
 }
 
-struct Editor {
-    reader: Reader,
-    output: Output,
-}
-
-impl Editor {
-    fn new() -> Self {
+impl CursorController {
+    fn new(win_size: (usize, usize)) -> CursorController {
         Self {
-            reader: Reader,
-            output: Output::new(),
-        }
-    }
-
-    fn process_keypress(&mut self) -> crossterm::Result<bool> {
-        match self.reader.read_key()? {
-            KeyEvent {
-                code: KeyCode::Char('q'),
-                modifiers: KeyModifiers::CONTROL,
-                ..
-            } => return Ok(false),
-            KeyEvent {
-                code: direction @ (KeyCode::Up | KeyCode::Down | KeyCode::Left | KeyCode::Right),
-                modifiers: KeyModifiers::NONE,
-                ..
-            } => self.output.move_cursor(direction),
-            _ => {}
-        }
-        Ok(true)
-    }
-
-    fn run(&mut self) -> crossterm::Result<bool> {
-        self.output.refresh_screen()?;
-        self.process_keypress()
-    }
-}
-
-struct Output {
-    win_size: (usize, usize),
-    editor_contents: EditorContents,
-    cursor_controller: CursorController,
-}
-
-impl Output {
-    fn new() -> Self {
-        let win_size = terminal::size()
-            .map(|(x, y)| (x as usize, y as usize))
-            .unwrap();
-        Self {
-            win_size,
-            editor_contents: EditorContents::new(),
-            cursor_controller: CursorController::new(win_size),
-        }
-    }
-
-    fn clear_screen() -> crossterm::Result<()> {
-        execute!(stdout(), terminal::Clear(ClearType::All))?;
-        execute!(stdout(), cursor::MoveTo(0, 0))
-    }
-
-    fn refresh_screen(&mut self) -> crossterm::Result<()> {
-        queue!(self.editor_contents, cursor::Hide, cursor::MoveTo(0, 0))?;
-        self.draw_rows();
-        let cursor_x = self.cursor_controller.cursor_x;
-        let cursor_y = self.cursor_controller.cursor_y;
-        queue!(
-            self.editor_contents,
-            cursor::MoveTo(cursor_x as u16, cursor_y as u16),
-            cursor::Show
-        )?;
-        self.editor_contents.flush()
-    }
-
-    fn draw_rows(&mut self) {
-        let screen_rows = self.win_size.1;
-        let screen_columns = self.win_size.0;
-        for i in 0..screen_rows {
-            if i == screen_rows / 3 {
-                let mut welcome = format!("Rubicon Editor --- Version {}", 0.2);
-                if welcome.len() > screen_columns {
-                    welcome.truncate(screen_columns)
-                }
-                let mut padding = (screen_columns - welcome.len()) / 2;
-                if padding != 0 {
-                    self.editor_contents.push('~');
-                    padding -= 1
-                }
-                (0..padding).for_each(|_| self.editor_contents.push(' '));
-                self.editor_contents.push_str(&welcome);
-            } else {
-                self.editor_contents.push('~');
-            }
-            queue!(
-                self.editor_contents,
-                terminal::Clear(ClearType::UntilNewLine)
-            )
-            .unwrap();
-            if i < screen_rows - 1 {
-                self.editor_contents.push_str("\r\n");
-            }
+            cursor_x: 0,
+            cursor_y: 0,
+            screen_columns: win_size.0,
+            screen_rows: win_size.1,
         }
     }
 
     fn move_cursor(&mut self, direction: KeyCode) {
-        self.cursor_controller.move_cursor(direction);
+        match direction {
+            KeyCode::Up => {
+                self.cursor_y = self.cursor_y.saturating_sub(1);
+            }
+            KeyCode::Left => {
+                if self.cursor_x != 0 {
+                    self.cursor_x -= 1;
+                }
+            }
+            KeyCode::Down => {
+                if self.cursor_y != self.screen_rows - 1 {
+                    self.cursor_y += 1;
+                }
+            }
+            KeyCode::Right => {
+                if self.cursor_x != self.screen_columns - 1 {
+                    self.cursor_x += 1;
+                }
+            }
+            /* add the following*/
+            KeyCode::End => self.cursor_x = self.screen_columns - 1,
+            KeyCode::Home => self.cursor_x = 0,
+            _ => unimplemented!(),
+        }
     }
 }
 
@@ -174,45 +100,140 @@ impl io::Write for EditorContents {
     }
 }
 
-struct CursorController {
-    cursor_x: usize,
-    cursor_y: usize,
-    screen_columns: usize,
-    screen_rows: usize,
+struct Output {
+    win_size: (usize, usize),
+    editor_contents: EditorContents,
+    cursor_controller: CursorController,
 }
 
-impl CursorController {
-    fn new(win_size: (usize, usize)) -> CursorController {
+impl Output {
+    fn new() -> Self {
+        let win_size = terminal::size()
+            .map(|(x, y)| (x as usize, y as usize))
+            .unwrap();
         Self {
-            cursor_x: 0,
-            cursor_y: 0,
-            screen_columns: win_size.0,
-            screen_rows: win_size.1,
+            win_size,
+            editor_contents: EditorContents::new(),
+            cursor_controller: CursorController::new(win_size), /* modify initializer*/
+        }
+    }
+
+    fn clear_screen() -> crossterm::Result<()> {
+        execute!(stdout(), terminal::Clear(ClearType::All))?;
+        execute!(stdout(), cursor::MoveTo(0, 0))
+    }
+
+    fn draw_rows(&mut self) {
+        let screen_rows = self.win_size.1;
+        let screen_columns = self.win_size.0;
+        for i in 0..screen_rows {
+            if i == screen_rows / 3 {
+                let mut welcome = format!("Rubicon Editor --- Version {}", VERSION);
+                if welcome.len() > screen_columns {
+                    welcome.truncate(screen_columns)
+                }
+                let mut padding = (screen_columns - welcome.len()) / 2;
+                if padding != 0 {
+                    self.editor_contents.push('~');
+                    padding -= 1
+                }
+                (0..padding).for_each(|_| self.editor_contents.push(' '));
+                self.editor_contents.push_str(&welcome);
+            } else {
+                self.editor_contents.push('~');
+            }
+            queue!(
+                self.editor_contents,
+                terminal::Clear(ClearType::UntilNewLine)
+            )
+                .unwrap();
+            if i < screen_rows - 1 {
+                self.editor_contents.push_str("\r\n");
+            }
         }
     }
 
     fn move_cursor(&mut self, direction: KeyCode) {
-        match direction {
-            KeyCode::Up => {
-                self.cursor_y = self.cursor_y.saturating_sub(1);
-            }
-            KeyCode::Left => {
-                if self.cursor_x != 0 {
-                    self.cursor_x -= 1;
+        self.cursor_controller.move_cursor(direction);
+    }
+
+    fn refresh_screen(&mut self) -> crossterm::Result<()> {
+        queue!(self.editor_contents, cursor::Hide, cursor::MoveTo(0, 0))?;
+        self.draw_rows();
+        let cursor_x = self.cursor_controller.cursor_x;
+        let cursor_y = self.cursor_controller.cursor_y;
+        queue!(
+            self.editor_contents,
+            cursor::MoveTo(cursor_x as u16, cursor_y as u16),
+            cursor::Show
+        )?;
+        self.editor_contents.flush()
+    }
+}
+
+struct Reader;
+
+impl Reader {
+    fn read_key(&self) -> crossterm::Result<KeyEvent> {
+        loop {
+            if event::poll(Duration::from_millis(500))? {
+                if let Event::Key(event) = event::read()? {
+                    return Ok(event);
                 }
             }
-            KeyCode::Down => {
-                if self.cursor_y != self.screen_columns - 1 {
-                    self.cursor_y += 1;
-                }
-            }
-            KeyCode::Right => {
-                if self.cursor_x != self.screen_columns - 1 {
-                    self.cursor_x += 1;
-                }
-            }
-            _ => unimplemented!(),
         }
+    }
+}
+
+struct Editor {
+    reader: Reader,
+    output: Output,
+}
+
+impl Editor {
+    fn new() -> Self {
+        Self {
+            reader: Reader,
+            output: Output::new(),
+        }
+    }
+
+    fn process_keypress(&mut self) -> crossterm::Result<bool> {
+        match self.reader.read_key()? {
+            KeyEvent {
+                code: KeyCode::Char('q'),
+                modifiers: KeyModifiers::CONTROL, ..
+            } => return Ok(false),
+            KeyEvent {
+                code:
+                direction
+                @
+                (KeyCode::Up
+                | KeyCode::Down
+                | KeyCode::Left
+                | KeyCode::Right
+                | KeyCode::Home
+                | KeyCode::End),
+                modifiers: KeyModifiers::NONE, ..
+            } => self.output.move_cursor(direction),
+            KeyEvent {
+                code: val @ (KeyCode::PageUp | KeyCode::PageDown),
+                modifiers: KeyModifiers::NONE, ..
+            } => (0..self.output.win_size.1).for_each(|_| {
+                self.output.move_cursor(if matches!(val, KeyCode::PageUp) {
+                    KeyCode::Up
+                } else {
+                    KeyCode::Down
+                });
+            }),
+            _ => {}
+        }
+        Ok(true)
+    }
+
+    fn run(&mut self) -> crossterm::Result<bool> {
+        self.output.refresh_screen()?;
+        self.process_keypress()
     }
 }
 
